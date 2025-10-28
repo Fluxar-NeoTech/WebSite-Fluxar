08d8c9b20a362fdd36b0d06ac5230385ea465273
Diogobq7 <diogo.queiroz@germinare.org.br>
2025-10-28 08:23:31 -0300

    feat: subindo visualização de senha

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,22 +1,48 @@
-import { InputContainer, Icon, StyledInput } from "./styles";
+import { useState } from 'react';
+import { InputContainer, Icon, StyledInput, ToggleButton } from "./styles";
+import EyeIcon from "../../assets/eye_icon.svg";
+import ClosedEyeIcon from "../../assets/closed_eye_icon.svg";
 
 export default function Input({ placeholder, value, onChange, type, icon }) {
+    const [showPassword, setShowPassword] = useState(false);
+    const isPassword = type === 'password';
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const inputType = isPassword ? (showPassword ? 'text' : 'password') : type;
+
     return (
         <InputContainer>
             <Icon>
                 <img
-                src={icon}
-                alt="email icon"
-                style={{ width: 20, height: 20 }}
-            />
+                    src={icon}
+                    alt="email icon"
+                    style={{ width: 20, height: 20 }}
+                />
             </Icon>
             <StyledInput
-                type={type}
+                type={inputType}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
                 required
+                isPassword={isPassword}
             />
+            {isPassword && (
+                <ToggleButton 
+                    type="button" 
+                    onClick={togglePasswordVisibility}
+                    aria-label={showPassword ? "Ocultar senha" : "Mostrar senha"}
+                >
+                    <img
+                        src={showPassword ? ClosedEyeIcon : EyeIcon}
+                        alt={showPassword ? "Ocultar senha" : "Mostrar senha"}
+                        style={{ width: 20, height: 20 }}
+                    />
+                </ToggleButton>
+            )}
         </InputContainer>
     )
 }
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,13 +2,13 @@ import styled from "styled-components";
 import { breakpoints } from "../../styles/breakpoints";
 
 export const InputContainer = styled.div`
+    position: relative;
     display: flex;
     align-items: center;
     border: 0.5px solid #f39c12;
     border-radius: 7px;
     padding: 12px 15px;
     width: 420px;
-
     transition: box-shadow 0.3s;
 
     &:focus-within {
@@ -35,7 +35,8 @@ export const StyledInput = styled.input`
   color: #333;
   background: transparent;
   width: 100%;
-  padding: none;
+  padding: 0;
+  padding-right: ${props => props.isPassword ? '30px' : '0'};
 
   &::placeholder {
     color: #aaa;
@@ -44,4 +45,47 @@ export const StyledInput = styled.input`
   &:focus {
     outline: none;
   }
+
+  &:-webkit-autofill,
+  &:-webkit-autofill:hover, 
+  &:-webkit-autofill:focus {
+    -webkit-text-fill-color: #333;
+    -webkit-box-shadow: 0 0 0px 1000px transparent inset;
+    transition: background-color 5000s ease-in-out 0s;
+  }
+
+  &:-moz-ui-invalid {
+    box-shadow: none;
+  }
+`;
+
+export const ToggleButton = styled.button`
+  position: absolute;
+  right: 15px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 18px;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+  
+  &:focus {
+    outline: 2px solid #f39c12;
+    outline-offset: 2px;
+    border-radius: 4px;
+  }
+  
+  &:hover {
+    opacity: 0.7;
+  }
+
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
 `;
